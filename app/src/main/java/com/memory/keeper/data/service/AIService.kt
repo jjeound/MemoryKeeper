23afa6a70d8ed9cec8a2d9edbe2d745168d55a56
iamgonnabe <126941459+iamgonnabe@users.noreply.github.com>
2025-07-11 17:22:57 +0900

    ai api

--- app/src/main/java/com/memory/keeper/data/service/AIService.kt
+++ app/src/main/java/com/memory/keeper/data/service/AIService.kt
@@ -5,8 +5,12 @@ import com.memory.keeper.data.dto.request.SpeechRequest
 import com.memory.keeper.data.dto.response.AIChatResponse
 import com.memory.keeper.data.dto.response.ApiResponse
 import com.memory.keeper.data.dto.response.Conversations
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.http.Body
+import retrofit2.http.Multipart
 import retrofit2.http.POST
+import retrofit2.http.Part
 
 interface AIService {
     @POST("/api/chat/start")
@@ -18,4 +22,11 @@ interface AIService {
     suspend fun getConversationByDate(
         @Body conversationRequest: ConversationRequest
     ): ApiResponse<Conversations>
+
+    @Multipart
+    @POST("/generate-video")
+    suspend fun generateVideo(
+        @Part("prompt") prompt: RequestBody,
+        @Part image: MultipartBody.Part
+    ): ApiResponse<String>
 }
